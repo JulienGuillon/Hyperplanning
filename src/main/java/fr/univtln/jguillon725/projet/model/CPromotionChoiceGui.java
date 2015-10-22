631a1a266b098a4cbed643d093428764290a9765
Julien Guillon <julien.guillon3@gmail.com>
2015-10-22 17:01:50 +0200

    modifié:         .gitignore 	nouveau fichier: src/main/java/fr/univtln/jguillon725/projet/model/CPromotionChoiceGui.form 	nouveau fichier: src/main/java/fr/univtln/jguillon725/projet/model/CPromotionChoiceGui.java 	nouveau fichier: src/main/java/fr/univtln/jguillon725/projet/model/CReservationGui.form 	nouveau fichier: src/main/java/fr/univtln/jguillon725/projet/model/CReservationGui.java 	modifié:         src/main/resources/creation.sql

---
+++ src/main/java/fr/univtln/jguillon725/projet/model/CPromotionChoiceGui.java
@@ -0,0 +1,30 @@
+package fr.univtln.jguillon725.projet.model;
+
+import fr.univtln.jguillon725.projet.exceptions.PersistanceException;
+import fr.univtln.jguillon725.projet.ihm.IView;
+
+import javax.swing.*;
+
+/**
+ * Created by julien on 22/10/15.
+ */
+public class CPromotionChoiceGui extends JFrame implements IView{
+    private JComboBox comboBox1;
+    private JButton validerButton;
+    private JPanel panelPromotionChoice;
+
+    @Override
+    public void createView(Object iModel) throws PersistanceException {
+
+    }
+
+    public CPromotionChoiceGui()
+    {
+        super("Choix promotion");
+        setSize(800, 600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setContentPane(panelPromotionChoice);
+
+        this.setVisible(true);
+    }
+}
---
+++ src/main/java/fr/univtln/jguillon725/projet/model/CReservationGui.java
@@ -0,0 +1,13 @@
+package fr.univtln.jguillon725.projet.model;
+
+import javax.swing.*;
+
+/**
+ * Created by julien on 22/10/15.
+ */
+public class CReservationGui {
+    private JComboBox comboBox1;
+    private JButton button1;
+    private JPasswordField passwordField1;
+    private JPanel panelReservation;
+}
